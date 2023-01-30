54ca29c22702930b8ceda3e4d280f5da21dc5220
Charlemagne <charlemagneapm1@gmail.com>
2023-01-29 19:14:59 -0800

    Figured out how to save qr code into downloads folder (Mac/linux-based) so not sure about windows... now figure out GUI

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import qrcode
 
+from pathlib import Path
+import os
 
 def main():
     urlToMakeCode = input("Please copy and paste a website url that you would like converted to a QR code: ")
@@ -7,13 +9,25 @@ def main():
     img = qrcode.make(urlToMakeCode)
     type(img)
 
-    ImageName = input("Now, please enter a name that the QR code will be saved as (will save as a png photo type): ")
+    imageName = input("Now, please enter a name that the QR code will be saved as (will save as a png photo type): ")
+    imageNameWithType = imageName + ".png"
 
-    img.save(ImageName + ".png")
+    img.save(imageNameWithType)
 
     if img:
-        print("A QR code has been created! Check the curreny directory (checking purposes for now)")
-        print("It has been saved as: " + ImageName + ".png \n")
+        print("A QR code has been created! Check the current directory (checking purposes for now)")
+        print("It has been saved as: " + imageName + ".png \n")
+
+    # NOT SURE IF WHAT'S BELOW WILL WORK FOR NON-LINUX LIKE WINDOWS (cuz this works for linux based like Mac)
+    # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+    saveInDownloads = input("Would you like this QR code saved to your downloads folder? ('Y' for yes, 'N' for no): ")
+    if saveInDownloads == "Y":
+        # https://stackoverflow.com/questions/35851281/python-finding-the-users-downloads-folder
+        path = str(os.path.join(Path.home(), "Downloads", imageNameWithType))
+        # https://stackoverflow.com/questions/31434278/how-do-i-use-python-pil-to-save-an-image-to-a-particular-directory
+        img.save(path, "PNG")
+
+
 
 
 
